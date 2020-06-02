d0a8187a297b2ecccfc78fd51876b41bf93d7b72
fanuelalem <57451397+fanuelalem@users.noreply.github.com>
2020-06-01 18:09:00 -0700

    Revert "added boiler plate"

--- client/src/actions/auth/index.js
+++
@@ -1,9 +0,0 @@
-import { AUTH_USER } from '../types';
-
-export const signOut = () => {
-  localStorage.removeItem('token');
-  return {
-    type: AUTH_USER,
-    payload: ''
-  };
-}
--- models/index.js
+++
@@ -1,7 +0,0 @@
-const Todo = require('./Todo');
-const User = require('./User');
-
-module.exports = {
-  Todo,
-  User,
-};
