1c5fd7fbb0529deee2b2aa215d387726c114dbb0
Husam Ahalim <s.ahalim@yahoo.com>
2020-06-10 20:34:48 -0700

    minor styling changes

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -4,7 +4,7 @@ class home extends Component {
     render() {
         return (
             <div>
-                <h1>home page</h1>
+                <h1>Home page</h1>
             </div>
         )
     }
