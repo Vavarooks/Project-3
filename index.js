16601350c5bae03d78777aace22d1ce47edb2a79
fanuelnalem <fanuelnalem@outlook.com>
2020-06-16 21:10:05 -0700

    FErf

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-if (process.env.NODE_ENV === 'production') {
+ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
- 
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.use("/",routes);
 require('./services/passport');
 // Connect database
