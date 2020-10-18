aa6e14a3a8850abe79c1aefe7e253fdd86154257
Fanuel Alem <fanuelnalem@outlook.com>
2020-10-18 12:59:59 -0700

    CW

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(express.json());
 
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
